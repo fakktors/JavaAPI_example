ed4847b5676b1903f951c3e83500955b44d88407
evsavin <162143148+evsavin@users.noreply.github.com>
2024-05-02 17:05:58 +0300

    EX 3

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,6 +4,6 @@ public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello, World!");
+        System.out.println("Hello from Evgenii");
     }
 }
