2e4edba2be5e6ebec1e80be7cd2b1c55938638b3
evsavin <fakktorss@gmail.com>
2024-05-11 21:03:11 +0300

    New ex * Ex 5 Done * Ex 6 Done * Ex 7 to-do ...

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,14 +1,56 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
+    public void testRestAssured(){
         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
+                .given()
+                .queryParam("name", "John")
+                .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
+    }
+
+    @Test
+    public void testEx5(){
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String answer = response.get("messages[1].message");
+        System.out.println(answer);
+    }
+
+    @Test
+    public void testEx6(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String location = response.getHeader("Location");
+        System.out.println(location);
+    }
+
+    // В разработке To-Do
+    @Test
+    public void testEx7(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String location = response.getHeader("Location");
+        System.out.println(location);
     }
 }
