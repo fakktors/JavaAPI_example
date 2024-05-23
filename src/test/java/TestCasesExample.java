8d3ebfc4cb4a1ae8ea62d23bfaf29798758cc3cd
evsavin <fakktorss@gmail.com>
2024-05-23 17:02:10 +0500

    * Ex11 done

--- src/test/java/TestCasesExample.java
+++ src/test/java/TestCasesExample.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestCasesExample {
 
     String urlLongMethod = "https://playground.learnqa.ru/api/hello";
+    String getCookieUrl = "https://playground.learnqa.ru/api/homework_cookie";
+    String cookieForTest = "hw_value";
 
     @Test
     public void testLongMethod(){
@@ -18,6 +21,19 @@ public class TestCasesExample {
                 .jsonPath();
 
         String envString = response.get("answer");
+
         assertTrue(envString.length() > 15, "String length is not greater than 15 characters");
+    }
+
+    @Test
+    public void testCookieValue(){
+
+        Response responseCookie = RestAssured
+                .get(getCookieUrl)
+                .andReturn();
+
+        String getCookie = responseCookie.getCookie("HomeWork");
+
+        assertEquals(cookieForTest, getCookie, "Unexpected cookie");
     }
 }
